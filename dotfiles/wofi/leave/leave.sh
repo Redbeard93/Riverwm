13505d7e81ff312722d3997658b6940f119c6c09
ENKS <67786490+EN-KS@users.noreply.github.com>
2022-04-27 01:40:41 +0800

    Update leave.sh

--- dotfiles/wofi/leave/leave.sh
+++ dotfiles/wofi/leave/leave.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-choice=$(printf "Lock\nLogout\nSuspend\nReboot\nShutdown" | wofi --dmenu --width 1 --height 212)
+choice=$(printf "Lock\nLogout\nSuspend\nReboot\nShutdown" | wofi --dmenu --width 1 --height 212 --prompt Power --insensitive 1 --location 1)
 if [[ $choice == "Lock" ]];then
     bash ~/.config/system_scripts/wayland_session_lock
 elif [[ $choice == "Logout" ]];then
