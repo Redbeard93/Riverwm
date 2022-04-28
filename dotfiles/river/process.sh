575d1a6b54eb0e7ff5380e5c630a8bf54ffe2ee5
ENKS <67786490+EN-KS@users.noreply.github.com>
2022-04-29 01:30:00 +0800

    Delete dotfiles directory

--- dotfiles/river/process.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# auto starting apps
-killall -q waybar dunst nm-applet
-waybar -c ~/.config/waybar/river/config-river -s ~/.config/waybar/river/river_style.css &
-dunst -config ~/.config/dunst/dunstrc &
-sh ~/.config/system_scripts/pkill_bc &
-nm-applet --indicator &
-swayidle -w timeout 300 ~/.config/system_scripts/wayland_session_lock &
